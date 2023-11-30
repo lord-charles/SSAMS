6792ee41b234862f968505fc5788821abb9acccf
lord-charles <121726049+lord-charles@users.noreply.github.com>
2023-11-30 04:19:27 -0800

    Add files via upload

--- navigation/EnrolmentStack.js
+++ navigation/EnrolmentStack.js
@@ -4,6 +4,7 @@ import HeaderRight from "../components/UI/HeaderRight";
 import HeaderLeft from "../components/UI/HeaderLeft";
 import AddStudent from "../screens/enrolment/AddStudent";
 import Enrolment from "../screens/enrolment/Enrolment";
+import StudentFlatlist from "../screens/enrolment/StudentFlatlist";
 
 const EnrolmentStack = () => {
   const EnrolmentStack = createNativeStackNavigator();
@@ -11,24 +12,30 @@ const EnrolmentStack = () => {
   return (
     <EnrolmentStack.Navigator
       screenOptions={{
-        
         headerShown: true,
       }}
     >
       {/* nested screens such as basic school info */}
       <EnrolmentStack.Screen
-      options={{
-        headerShown: false
-      }}
+        options={{
+          headerShown: false,
+        }}
         name="Enrolment"
         component={Enrolment}
       />
       <EnrolmentStack.Screen
-      options={{
-        title: "Add New Student"
-      }}
+        options={{
+          title: "Add New Student",
+        }}
         name="AddStudent"
         component={AddStudent}
+      />
+      <EnrolmentStack.Screen
+        options={{
+          title: "Student List",
+        }}
+        name="StudentList"
+        component={StudentFlatlist}
       />
     </EnrolmentStack.Navigator>
   );
